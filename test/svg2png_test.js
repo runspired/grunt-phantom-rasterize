5acad5c0aa9d6a7b283e2794458e2ceb0c1fb86a
Chris Thoburn <runspired@gmail.com>
2014-07-23 20:36:20 -0500

    cleaned up formatting and expanded descriptions of tests

--- test/svg2png_test.js
+++ test/svg2png_test.js
@@ -1,32 +1,30 @@
-
-"use strict";
-
-var fs = require("fs");
-var assert = require("assert");
-var sizeOf = require("image-size");
+/*global require, describe, it*/
+var fs = require("fs"),
+    assert = require("assert"),
+    sizeOf = require("image-size");
 
 describe("svg2png", function () {
 
-  it("Creates subdir", function () {
-    assert(fs.existsSync("test/svg/png"));
-    assert(fs.existsSync("test/svg/png_2x"));
-  });
+    it("Creats Subdirectory for new icons", function () {
+        assert(fs.existsSync("test/svg/png"));
+        assert(fs.existsSync("test/svg/png_2x"));
+    });
 
-  it("Generates PNG", function () {
-    assert(fs.existsSync("test/svg/png/test.png"));
-    assert(fs.existsSync("test/svg/png_2x/test.png"));
-  });
+    it("Generates PNG", function () {
+        assert(fs.existsSync("test/svg/png/test.png"));
+        assert(fs.existsSync("test/svg/png_2x/test.png"));
+    });
 
-  it("Generates 1x PNG", function () {
-    var dimensions = sizeOf("test/svg/png/test.png");
-    assert(dimensions.width === 60);
-    assert(dimensions.height === 60);
-  });
+    it("Generates PNG from fallback options.", function () {
+        var dimensions = sizeOf("test/svg/png/test.png");
+        assert(dimensions.width === 60);
+        assert(dimensions.height === 60);
+    });
 
-  it("Generates 2x PNG", function () {
-    var dimensions = sizeOf("test/svg/png_2x/test.png");
-    assert(dimensions.width === 128);
-    assert(dimensions.height === 128);
-  });
+    it("Generates PNG from specified options.", function () {
+        var dimensions = sizeOf("test/svg/png_2x/test.png");
+        assert(dimensions.width === 128);
+        assert(dimensions.height === 128);
+    });
 
 });
