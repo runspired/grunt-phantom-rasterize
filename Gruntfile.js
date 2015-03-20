dad1c6f2a7395bb6a6d87adab90973c7175377bf
Chris Thoburn <runspired@gmail.com>
2015-03-19 20:36:01 -0500

    add automated release process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,24 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
 
+    pkg : grunt.file.readJSON("package.json"),
+
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: ['pkg'],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'upstream',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
+    },
+
     rasterize : {
       fallback: {
         options : {
@@ -59,5 +77,38 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-simple-mocha");
 
   grunt.registerTask("default", ["clean", "rasterize", "simplemocha", "clean"]);
+
+
+  /*
+   Generate a Release (also creates a build);
+   */
+  grunt.registerTask(
+    'release',
+    'Creates and Publishes a Versioned Release. First arg is target, second arg allows for specific environment.',
+    function (target) {
+
+      grunt.loadNpmTasks('grunt-bump');
+      var shouldBump = !!target;
+
+      if (!shouldBump) {
+        grunt.log.warn('[WARNING] grunt:release – No arguments provided. Version will not be bumped.');
+      }
+
+      if (shouldBump && !~['patch', 'major', 'minor', 'prerelease', 'git'].indexOf(target)) {
+        grunt.log.error('[ERROR] grunt:release – "' + target + '" is not a valid semver target for to bump.');
+        return false;
+      }
+
+      if (shouldBump) {
+        grunt.task.run(['bump-only:' + target]);
+      }
+
+      grunt.task.run([
+        'bump-commit'
+      ]);
+
+    }
+  );
+
 
 };
